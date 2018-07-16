30a2eb839ed82544bdabe281d369cffee48d1e15
DIRECTORY\jong_jn <jurjen.dejong@deltares.nl>
2018-07-16 13:35:42 +0200

    Polygon to network functions

--- eo_river/eo_river.py
+++ eo_river/eo_river.py
@@ -1,3 +1,36 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
+
+
+def generate_network(watermask):
+    """    This function loads a watermask, skeletonise and return as a network
+    of branches.
+
+    Args:
+       watermask: geojson-string or -file with only the polygon of the river
+       output: write output to file instead of returning output as string
+
+    Output:
+       network: geojson-string or -file with the network
+    """
+    import ratin.network as nw
+
+    STARTEND = [[197727.048,503291.164], [195687.574,504118.171]]  # This should be derived automatically!
+
+    SMOOTHDEGREE, cutpoints, densi, space, horiLines = 15.0, 20, 5.0, 5.0, 15  #dense grid
+
+    # This now works for single branch
+    network = Network()
+    network.load_geometries(polygons=watermask, se_points=STARTEND)
+    network.densify(densi)
+    network.construct(spacing=[space])
+    network.delete(branch='1', num_of_pnts=cutpoints)
+    #Representation
+    network.plot(1)
+    #Network Stats:
+    network.overview(branch='1', fignum=2, smoothdegree=SMOOTHDEGREE, printoutput=False, units='radians')
+    #Save output of network file
+    # network.save('output/network')
+
+    return None
