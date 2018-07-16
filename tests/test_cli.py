0aa5593c6a61202633ca6d5c9dc8dbb5cef80cf2
gena <gennadiy.donchyts@gmail.com>
2018-07-16 18:09:57 +0200

    fix build

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -31,7 +31,7 @@ def test_command_line_interface():
     runner = CliRunner()
     result = runner.invoke(cli.main)
     assert result.exit_code == 0
-    assert 'eo_river.cli.main' in result.output
+    assert 'get-water-mask' in result.output
     help_result = runner.invoke(cli.main, ['--help'])
     assert help_result.exit_code == 0
     assert '--help  Show this message and exit.' in help_result.output
